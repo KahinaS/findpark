c2acdb840274830658d6266be776c5840a77b2cd
Ludovic Delmas <ludovic.delmas34@gmail.com>
2020-04-10 17:39:19 +0200

    first commit

--- index.php
+++ index.php
@@ -1,110 +1,138 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
+	<meta charset="UTF-8">
 
-  <title>FindPark</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/styles.css">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<title>FindPark</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
- 
+
 </head>
 
 <body>
-  <nav class="navbar">
-    <h1 class="navbar ml-2">FindPark <p>Montpellier</p></h2>
-    
-    <div class="d-flex justify-content-end">
+	<nav class="navbar">
+		<h1 class="navbar ml-2">FindPark <p>Montpellier</p>
+			</h1>
 
-      <button class="mr-4 rounded ">
-        <a class="nav-link ">sign in</a>
-      </button>
-      <button class="rounded">
-        <a class="nav-link">sign up</a>
-      </button>
+			<div class="search">
+				<p>Rechercher</p>
+				<input class="pt-2 w-100" type="text" placeholder="..">
+			</div>
+			
 
-    </div>
-  </nav>
-  <div class="btn-group btn-group-toggle w-100 " data-toggle="buttons">
-    <label id="color1" class="btn w-50">
-      <input type="radio" name="options" id="option1" checked> Arc de Triomphe 
-    </label>
-    <label id="color" class="btn w-50">
-      <input type="radio" name="options" id="option1" > Pitot
-    </label>
-  </div>
-  <div class="btn-group btn-group-toggle w-100 " data-toggle="buttons">
-    <label id="color" class="btn w-50 active">
-      <input class="button" type="radio" name="options" id="option1" checked> Comédie
-    </label>
-    <label id="color1" class="btn w-50 ">
-      <input type="radio" name="options" id="option1">  Arceaux
-    </label>
-  </div>
-  <div class="btn-group btn-group-toggle w-100 " data-toggle="buttons">
-    <label id="color1" class="btn w-50">
-      <input type="radio" name="options" id="option1" checked>  Corum
-    </label>
-    <label id="color" class="btn w-50">
-      <input type="radio" name="options" id="option1">  Nombre d'Or
-    </label>
-  </div>
-  <div class="w3-container">
-  
-    
-    <button onclick="myFunction('Demo1')" class="w3-btn w3-block w3-black w3">
-    <img src="images/triangle.png"></button>
-    
-    <div id="Demo1" class="w3-hide ">
-      <a class="w3-button btn-group btn-group-toggle w-100 p-0">
-        <label id="color" class="btn w-50">
-          <input type="radio" name="options" id="option1"> Polygone
-        </label>
-        <label  id="color1" class="btn w-50">
-          <input  type="radio" name="options" id="option1" checked>  Europa
-        </label>
-       
-      </a>
-      <a class="w3-button btn-group btn-group-toggle w-100 p-0" >
-        <label  id="color1" class="btn w-50">
-          <input  type="radio" name="options" id="option1" checked>  Foch Préfecture
-        </label>
-        <label id="color" class="btn w-50">
-          <input type="radio" name="options" id="option1"> Gambetta
-        </label>
-      </a>
-      <a class="w3-button btn-group btn-group-toggle w-100 p-0" > 
-        <label id="color" class="btn w-50">
-          <input type="radio" name="options" id="option1"> Laissac
-        </label>
-        <label  id="color1" class="btn w-50">
-          <input  type="radio" name="options" id="option1" checked>  Triangle
-        </label>
-      
-      </a>
-    
-    </div>
-    
-    </div>
-   
-  </div>
+			<div class="d-flex justify-content-end">
+
+				<button class="mr-4 rounded ">
+					<a class="nav-link">sign in</a>
+				</button>
+				<button class="rounded">
+					<a class="nav-link">sign up</a>
+				</button>
+
+			</div>
+	</nav>
+
+	<div id="groupeBoutons">
+		<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+			<!-- <label id="color1" class="btn w-50">
+				<input type="radio" name="options" id="option1" checked> Arc de Triomphe
+			</label>
+			<label id="color" class="btn w-50">
+				<input type="radio" name="options" id="option1"> Pitot
+			</label> -->
+			<button class="btn color1 w-50" id="ARCT">Arc de Triomphe
+			</button>
+			<button class="btn color w-50" id="PITO">Pitot</button>
+		</div>
+
+		<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+			<!-- <label id="color" class="btn w-50 active">
+				<input class="button" type="radio" name="options" id="option1" checked> Comédie
+			</label>
+			<label id="color1" class="btn w-50 ">
+				<input type="radio" name="options" id="option1"> Arceaux
+			</label> -->
+			<button class="btn color w-50" id="COME">Comédie</button>
+			<button class="btn color1 w-50" id="arceaux">Arceaux (non disponible)</button> <!-- c'est le parking pitot ? -->
+		</div>
+
+		<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+			<!-- <label id="color1" class="btn w-50">
+				<input type="radio" name="options" id="option1" checked> Corum
+			</label>
+			<label id="color" class="btn w-50">
+				<input type="radio" name="options" id="option1"> Nombre d'Or
+			</label> -->
+			<button class="btn color1 w-50" id="CORU">Corum</button>
+			<button class="btn color w-50" id="or">Nombre d'Or (non disponible)</button> <!-- c'est le parking d'anitgone ? -->
+		</div>
+	</div>
+
+	<div class="w3-container">
+
+		<div id="Demo1" class="w3-hide ">
+			<!-- <a class="w3-button btn-group btn-group-toggle w-100 p-0">
+				<label id="color" class="btn w-50">
+					<input type="radio" name="options" id="option1"> Polygone
+				</label>
+				<label id="color1" class="btn w-50">
+					<input type="radio" name="options" id="option1" checked> Europa
+				</label>
+
+			</a> -->
+			<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+				<button class="btn color w-50" id="polygone">Polygone (non disponible)</button> <!-- Parking privé (non TAM) -->
+				<button class="btn color1 w-50" id="EURO">Europa</button>
+			</div>
+			<!-- <a class="w3-button btn-group btn-group-toggle w-100 p-0">
+				<label id="color1" class="btn w-50">
+					<input type="radio" name="options" id="option1" checked> Foch Préfecture
+				</label>
+				<label id="color" class="btn w-50">
+					<input type="radio" name="options" id="option1"> Gambetta
+				</label>
+			</a> -->
+			<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+				<button class="btn color1 w-50" id="FOCH">Foch Préfecture</button>
+				<button class="btn color w-50" id="GAMB">Gambetta</button>
+			</div>
+			<!-- <a class="w3-button btn-group btn-group-toggle w-100 p-0">
+				<label id="color" class="btn w-50">
+					<input type="radio" name="options" id="option1"> Laissac
+				</label>
+				<label id="color1" class="btn w-50">
+					<input type="radio" name="options" id="option1" checked> Triangle
+				</label>
+
+			</a> -->
+			<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
+				<button class="btn color w-50" id="laissac">Laissac (fermé)</button>
+				<button class="btn color1 w-50" id="TRIA">Triangle</button>
+			</div>
+
+		</div>
+		<button onclick="myFunction('Demo1')" class="w3-btn w3-block w3-black w3">
+			<img id="triangle" src="images/triangle.png"></button>
+
+	</div>
+
+	</div>
 
 
 
-      <div>
-      <iframe src="https://www.google.com/maps/d/embed?mid=1ES6UwanSkPVbl8RD6N6AJCLMIE8" width="100%" height="1590" frameborder="0" style="border:0;" allowfullscreen=""> </iframe>
-         
-      </div>
-      <div class="search"><p>Rechercher</p></div>
-      <input class="pt-2 w-100" type="text" placeholder="..">
-      <script src="script.js"></script>
-      <script src="app.js">
-        
-      </script>
+	<div id="map"></div>
+
+	<script src="script.js"></script>
+	<script src="app.js">
+
+	</script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?v=3.39&key=AIzaSyBv_PojW_2qKncUAIMj5kJXFEjCd0aIDWM&callback=initMap" type="text/javascript"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,16 +1,303 @@
 
-// const myData = 'http://data.montpellier3m.fr/sites/default/files/ressources/OSM_Juvignac_parking_point.json'
-// let park = fetch(myData)
-// .then(response => response.json())
-// .then(response => console.log(response.results))
-// .catch(error => console.log(error));
+let groupeBoutons1 = document.getElementById("groupeBoutons");
+let groupeBoutons2 = document.getElementById("Demo1");
+var triangle = 1;
 
 function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
- 
+	var t = document.getElementById("triangle");
+	var x = document.getElementById(id);
+	
+	if (triangle === 1){
+		t.setAttribute("src", "images/triangle2.png");
+		triangle = 2;
+	} else {
+		t.setAttribute("src", "images/triangle.png");
+		triangle = 1;
+	}
+
+	if (x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+
+function trouverPlacesLibres(e) {
+	let proxyurl = "https://cors-anywhere.herokuapp.com/";
+	let url = "http://data.montpellier3m.fr/sites/default/files/ressources/FR_MTP_" + e.target.id + ".xml";
+	let urlFinale = proxyurl + url;
+	let requeteHTTP = new XMLHttpRequest();
+	requeteHTTP.open("GET", urlFinale, false);
+	requeteHTTP.setRequestHeader("Conteent-Type", "text/xml");
+	requeteHTTP.send(null);
+	let reponse = requeteHTTP.responseXML;
+	if (reponse === null) {
+		alert("Données non disponibles");
+	} else {
+		let placesDisponibles = reponse.childNodes[0].children[3].textContent.toString();
+		let message = "Il y a " + placesDisponibles + " places libres dans le parking " + e.target.value + ".";
+		alert(message);
+	}
+}
+
+groupeBoutons1.onclick = trouverPlacesLibres;
+groupeBoutons2.onclick = trouverPlacesLibres;
+
+function trouverPlacesLibresMap(marker) {
+	let proxyurl = "https://cors-anywhere.herokuapp.com/";
+	let url = "http://data.montpellier3m.fr/sites/default/files/ressources/FR_MTP_" + marker.nomXML + ".xml";
+	let urlFinale = proxyurl + url;
+	let requeteHTTP = new XMLHttpRequest();
+	requeteHTTP.open("GET", urlFinale, false);
+	requeteHTTP.setRequestHeader("Conteent-Type", "text/xml");
+	requeteHTTP.send(null);
+	let reponse = requeteHTTP.responseXML;
+	if (reponse === null) {
+		alert("Données non disponibles");
+	} else {
+		let placesDisponibles = reponse.childNodes[0].children[3].textContent.toString();
+		let message = "Il y a " + placesDisponibles + " places libres dans le parking " + marker.title + ".";
+		alert(message);
+	}
+}
+
+
+function initMap() {
+	// The location of Montpellier
+	var Montpellier = {
+		lat: 43.6110887,
+		lng: 3.874522
+	};
+	// The map, centered at Montpellier
+	var map = new google.maps.Map(
+	document.getElementById('map'), {
+		zoom: 13,
+		center: Montpellier
+	});
+	
+	let icons = 'http://maps.google.com/mapfiles/kml/paddle/blu-circle.png';
+
+	var trafficLayer = new google.maps.TrafficLayer();
+	trafficLayer.setMap(map);
+
+	var marker_Anti = new google.maps.Marker({
+		position: {lat: 43.60889, lng:3.88807 },
+		map: map,
+		title: 'Antigone',
+		nomXML:'ANTI',
+		type: 'parking',
+		icon: icons
+	});
+
+	var marker_Come = new google.maps.Marker({
+		position: {lat: 43.6087, lng:3.88033 },
+		map: map,
+		title: 'Comedie',
+		nomXML:'COME',
+		icon: icons
+	});
+
+	var marker_Coru = new google.maps.Marker({
+		position: {lat: 43.61361, lng:3.88188 },
+		map: map,
+		title: 'Corum',
+		nomXML:'CORU',
+		icon: icons
+	});
+
+	var marker_Euro = new google.maps.Marker({
+		position: {lat: 43.60803, lng:3.89408 },
+		map: map,
+		title: 'Europa',
+		nomXML:'EURO',
+		icon: icons
+	});
+
+	var marker_Foch = new google.maps.Marker({
+		position: {lat: 43.61069, lng:3.87634 },
+		map: map,
+		title: 'Foch', 
+		nomXML:'FOCH',
+		icon: icons
+	});
+
+	var marker_Gamb = new google.maps.Marker({
+		position: {lat: 43.60865, lng:3.86753 },
+		map: map,
+		title: 'Gambetta', 
+		nomXML:'GAMB',
+		icon: icons
+	});
+
+	var marker_Gare = new google.maps.Marker({
+		position: {lat: 43.60362, lng:3.87899 },
+		map: map,
+		title: 'Gare', 
+		nomXML:'GARE',
+		icon: icons
+	});
+
+	var marker_Tria = new google.maps.Marker({
+		position: {lat: 43.60933, lng:3.88201 },
+		map: map,
+		title: 'Triangle', 
+		nomXML:'TRIA',
+		icon: icons
+	});
+
+	// The marker, positioned at Montpellier
+	var marker_Arct = new google.maps.Marker({
+		position: {lat: 43.61098, lng: 3.87312},
+		map: map,
+		title: 'Arc de triomphe',
+		nomXML: 'ARCT',
+		icon: icons
+	});
+
+	var marker_Pito = new google.maps.Marker({
+		position: {lat: 43.61265, lng: 3.8709},
+		map: map,
+		title: 'Pito',
+		nomXML: 'PITO',
+		icon: icons
+	});
+
+	var marker_Circ = new google.maps.Marker({
+		position: {lat: 43.60513, lng: 3.91962}, 
+		map: map,
+		title: 'Circé',
+		nomXML: 'CIRC',
+		icon: icons
+	});
+
+	var marker_Sabi = new google.maps.Marker({
+		position: {lat: 43.58488, lng: 3.86006},
+		map: map,
+		title: 'Sabines',
+		nomXML: 'SABI',
+		icon: icons
+	});
+
+	var marker_Garc = new google.maps.Marker({
+		position: {lat: 43.59051, lng: 3.89097},
+		map: map,
+		title: 'Garcia Lorca',
+		nomXML: 'GARC',
+		icon: icons
+	});
+
+	var marker_Moss = new google.maps.Marker({
+		position: {lat: 43.61648, lng: 3.81873},
+		map: map,
+		title: 'Mosson',
+		nomXML: 'MOSS',
+		icon: icons
+	});
+
+	var marker_Medc = new google.maps.Marker({
+		position: {lat: 43.63906, lng: 3.82818},
+		map: map,
+		title: 'Euromédecine',
+		nomXML: 'MEDC',
+		icon: icons
+	});
+
+	var marker_Occi = new google.maps.Marker({
+		position: {lat: 43.6357, lng: 3.84801},
+		map: map,
+		title: 'Occitanie',
+		nomXML: 'OCCI',
+		icon: icons
+	});
+
+	var marker_Ga109 = new google.maps.Marker({
+		position: {lat: 43.60413, lng: 3.91696},  
+		map: map,
+		title: 'Gaumont OUEST',
+		nomXML: 'GA109',
+		icon: icons
+	});
+
+	var marker_Ga250= new google.maps.Marker({
+		position: {lat: 43.6044, lng: 3.91428},
+		map: map,
+		title: 'Gaumont EST',
+		nomXML: 'GA250',
+		icon: icons
+	});
+
+	marker_Anti.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Anti);
+	});
+
+	marker_Come.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Come);
+	});
+
+	marker_Coru.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Coru);
+	});
+
+	marker_Euro.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Euro);
+	});
+
+	marker_Foch.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Foch);
+	});
+
+	marker_Gamb.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Gamb);
+	});
+
+	marker_Gare.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Gare);
+	});
+
+	marker_Tria.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Tria);
+	});
+
+	marker_Arct.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Arct);
+	});
+
+	marker_Pito.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Pito);
+	});
+
+	marker_Circ.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Circ);
+	});
+
+	marker_Sabi.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Sabi);
+	});
+
+	marker_Garc.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Garc);
+	});
+
+	marker_Moss.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Moss);
+	});
+
+	marker_Medc.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Medc);
+	});
+
+	marker_Occi.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Occi);
+	});
+
+	marker_Ga109.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Ga109);
+	});
+
+	marker_Ga250.addListener('click', function() {
+		trouverPlacesLibresMap(marker_Ga250);
+	});
+
+
+
+}
